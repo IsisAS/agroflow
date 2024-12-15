fcfce9d96023fdb1e3b980146c82ce9ae45725b0
IsisAS <144072352+IsisAS@users.noreply.github.com>
2024-12-15 17:46:25 -0300

    - Criação do dashboard - Lógica para alterar a cor do menu selecionado

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,10 @@
-export default function Dashboard(){
-    return(
+export default function Dashboard() {
+    return (
         <div>
-            <p>Dashboard</p>
+            <div>
+                <p className="text-[--text-color] text-[20px] font-bold">Bem-vindo ao AgroFlow Dashboard</p>
+                <p className="text-[--light-gray] font-semibold">Seu controle completo de recursos e dados no campo</p>
+            </div>
         </div>
     )
 }
